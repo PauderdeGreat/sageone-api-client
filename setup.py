27938617109f1bb2de90bfceccb3eade0cc07cd0
Panashe Mlambo <PauderdeGreat@users.noreply.github.com>
2023-04-12 20:14:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='sageone-api-client',
-    version='0.0.2',
+    version='1.0.0',
     description='Sage One API Client',
     author='Max Naude',
     author_email='maxnaude@gmail.com',
     url='https://github.com/maxnaude/sageone-api-client',
     packages=find_packages(),
     dependency_links=[],
-    install_requires=['hammock==0.2.4'],
+    install_requires=['hammock'],
     test_suite='nose.collector',
     tests_require=['nose', 'responses'],
     include_package_data=True,
